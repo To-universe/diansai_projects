c728dbe639e8f57f77cd85592d814412ca18952c
to-universe <2176342971@qq.com>
2026-07-22 14:55:42 +0800

    查表计算熵

--- YUANSAI/APP/Inc/entropy_calc.h
+++ YUANSAI/APP/Inc/entropy_calc.h
@@ -2,7 +2,9 @@
 #define __ENTROPY_CALC_H__
 
 #include <stdint.h>
+#include "receive_pic.h"
 
 
 void entropy_calc_all(uint8_t* gray_pic,float* entropy_abs,float* entropy_rel);
+extern const float entropy_count_lut[PIC_SIZE + 1U];
 #endif
