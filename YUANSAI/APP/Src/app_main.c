c29e1b26643033ebc607834999af662475e097b3
to-universe <2176342971@qq.com>
2026-07-22 10:11:51 +0800

    串口接受逻辑

--- YUANSAI/APP/Src/app_main.c
+++ YUANSAI/APP/Src/app_main.c
@@ -1,8 +1,24 @@
 #include "app_main.h"
+#include "receive_pic.h"
+#include "tjpgd.h"
 
 int app_main(void){
+    const uint8_t *jpg_data = 0;
+    uint16_t jpg_len = 0;
+
+    receive_pic_init();
 
     while (1) {
-    
+        receive_pic_poll();
+
+        if (receive_pic_is_frame_ready() != 0U) {
+            jpg_data = receive_pic_get_frame_data();
+            jpg_len = receive_pic_get_frame_len();
+
+            (void)jpg_data;
+            (void)jpg_len;
+
+            receive_pic_release_frame();
+        }
     }
 }
---
+++ YUANSAI/APP/Src/pic_decode.c
@@ -0,0 +1,10 @@
+#include "pic_decode.h"
+#include "tjpgd.h"
+#include <stdint.h>
+
+void decode_data(uint8_t* input,uint8_t* output){
+    JDEC jdec;
+    JRESULT res;
+
+    
+}
